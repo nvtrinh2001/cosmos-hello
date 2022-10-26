93f3b0be110db4fcb5573d8d810a188bafd43131
Trinh V. Nguyen <nguyenvantrinh2607@gmail.com>
2022-10-27 02:09:22 +0700

    send a message

--- x/hello/module.go
+++ x/hello/module.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	// this line is used by starport scaffolding # 1
 
+	"context"
 	"github.com/gorilla/mux"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"github.com/spf13/cobra"
@@ -78,6 +79,7 @@ func (AppModuleBasic) RegisterRESTRoutes(clientCtx client.Context, rtr *mux.Rout
 // RegisterGRPCGatewayRoutes registers the gRPC Gateway routes for the module.
 func (AppModuleBasic) RegisterGRPCGatewayRoutes(clientCtx client.Context, mux *runtime.ServeMux) {
 	// this line is used by starport scaffolding # 2
+	types.RegisterQueryHandlerClient(context.Background(), mux, types.NewQueryClient(clientCtx))
 }
 
 // GetTxCmd returns the capability module's root tx command.
